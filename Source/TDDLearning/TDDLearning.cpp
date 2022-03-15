c0fd1319e42cfa90617a09f913f6cfca02b93524
SSVILENO <SSVILENO@ford.com>
2022-03-15 17:06:00 +0100

    Project deleted

--- Source/TDDLearning/TDDLearning.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TDDLearning.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TDDLearning, "TDDLearning" );
- 
